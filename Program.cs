4e6e8359058f4f2a74064dd700159d56638a9496
SiGuy21 <100978842+SiGuy21@users.noreply.github.com>
2026-01-21 15:40:32 -0700

    Second Changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-// hi
+// This code snippet prompts the user to input details about a food item.
+Console.Write("Food Name: ");
+Console.ReadLine();
+Console.WriteLine();
+Console.Write("Food Category: ");
+Console.ReadLine();
+Console.WriteLine();
+Console.Write("Quantity: ");
+Console.ReadLine();
+Console.WriteLine();
+Console.Write("Expiration Date (MM/DD/YYYY): ");
+Console.ReadLine();
+Console.WriteLine();
