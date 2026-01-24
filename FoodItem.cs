9595ae3411098c63b10520eb6c4a0b631310f4c4
SiGuy21 <100978842+SiGuy21@users.noreply.github.com>
2026-01-23 19:25:08 -0700

    Next commit

--- FoodItem.cs
+++ FoodItem.cs
@@ -1,10 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace Mission3
 {
     internal class FoodItem
     {
+
+        // These are the properties of a food item that will be stored in the foodBank
+        public string foodName { get; set; }
+        public string foodCategory { get; set; }
+        public int foodQuantity { get; set; }
+        public DateTime foodExpiration { get; set; }
+
+        // This creates a new food item with the given parameters and stores them
+        public FoodItem(string foodName, string foodCategory, int foodQuantity, DateTime foodExpiration)
+        {
+            // Assign the parameters to the properties of the food item
+            this.foodName = foodName;
+            this.foodCategory = foodCategory;
+            this.foodQuantity = foodQuantity;
+            this.foodExpiration = foodExpiration;
+
+        }
+        // This gets the user input for a new food item and returns a new FoodItem object
+        public static FoodItem UserInput()
+        {
+            Console.WriteLine("\nAdd New Food Item");
+
+            Console.Write("Food Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Food Category: ");
+            string category = Console.ReadLine();
+
+            // Validate quantity input to ensure it's a positive integer
+            int quantity = 0;
+            bool validQuantity = false;
+            // Loop until a valid quantity is entered by the user
+            while (!validQuantity)
+            {
+                Console.Write("Quantity: ");
+                string quantityInput = Console.ReadLine();
+
+                // Try to parse the input as an integer and check if it's positive 
+                if (int.TryParse(quantityInput, out quantity) && quantity > 0)
+                {
+                    validQuantity = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid positive number.");
+                }
+            }
+
+            // Validate expiration date input to ensure it's a valid date
+            DateTime expirationDate = DateTime.Now;
+            bool validDate = false;
+            // Loop until a valid date is entered by the user
+            while (!validDate)
+            {
+                Console.Write("Expiration Date (MM/DD/YYYY): ");
+                string dateInput = Console.ReadLine();
+
+                if (DateTime.TryParse(dateInput, out expirationDate))
+                {
+                    validDate = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid date in MM/DD/YYYY format.");
+                }
+            }
+
+            return new FoodItem(name, category, quantity, expirationDate);
+        }
+        // This displays the food item information in a readable format 
+        public void showItems()
+        {
+            string expiredStatus = foodExpiration < DateTime.Now ? " [EXPIRED]" : "";
+            Console.WriteLine($"Name: {foodName}");
+            Console.WriteLine($"Category: {foodCategory}");
+            Console.WriteLine($"Quantity: {foodQuantity}");
+            Console.WriteLine($"Expiration Date: {foodExpiration.ToShortDateString()}{expiredStatus}");
+            Console.WriteLine();
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,130 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
 
-// This code snippet prompts the user to input details about a food item.
-Console.Write("Food Name: ");
-Console.ReadLine();
-Console.WriteLine();
-Console.Write("Food Category: ");
-Console.ReadLine();
-Console.WriteLine();
-Console.Write("Quantity: ");
-Console.ReadLine();
-Console.WriteLine();
-Console.Write("Expiration Date (MM/DD/YYYY): ");
-Console.ReadLine();
-Console.WriteLine();
+namespace Mission3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list to store all food items
+            List<FoodItem> foodBank = new List<FoodItem>();
+
+            // This is our true/flase statement for the user inputs
+            bool userAnswer = true;
+
+            // while loop that keeps the events going until the user exits the program
+            while (userAnswer)
+            {
+                // Options the user can make 
+                Console.WriteLine("1. Add Food Item");
+                Console.WriteLine("2. Delete Food Items");
+                Console.WriteLine("3. Print List of Current Food Items");
+                Console.WriteLine("4. Exit the Program");
+                Console.Write("\nEnter your choice (1-4): ");
+
+                // this gets the user input so it can be used for the options
+                string userChoice = Console.ReadLine();
+
+                // takes the user input and based on the number they input it'll run one of the events
+                switch (userChoice)
+                {
+                    case "1":
+                        AddFoodItem(foodBank);
+                        break;
+                    case "2":
+                        DeleteFoodItem(foodBank);
+                        break;
+                    case "3":
+                        inventoryList(foodBank);
+                        break;
+                    case "4":
+                        userAnswer = false;
+                        Console.WriteLine("\nThank you, Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid choice. Please choose a number between 1 and 4.");
+                        break;
+                }
+            }
+        }
+
+        // This adds a new food item to our foodBank
+        static void AddFoodItem(List<FoodItem> foodBank)
+        {
+            // holds the users input to be added to the foodBank
+            FoodItem newItem = FoodItem.UserInput();
+            foodBank.Add(newItem);
+            Console.WriteLine("Item has been added!");
+            Console.WriteLine();
+        }
+
+        // THis deletes an item from our foodBank
+        static void DeleteFoodItem(List<FoodItem> foodBank)
+        {
+            // Shows that there aren't any items in the inventory 
+            //if (foodBank.Count == 0)
+            //{
+            //    Console.WriteLine("\nThe inventory is empty. There are no items to delete.");
+            //    return;
+            //}
+            Console.WriteLine("\nDelete Food Item:");
+            Console.WriteLine("\nCurrent Inventory:");
+
+            // Display all the food items with their experation dates
+            for (int i = 0; i < foodBank.Count; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Food Name");
+                Console.WriteLine($"{i + 1}. {foodBank[i].foodName}");
+            }
+
+            // Gets the users choice and deletes item accordingly
+            Console.Write("\nEnter the number of the item to delete (or 0 to cancel): ");
+            string input = Console.ReadLine();
+
+            // if the user enters 0 the delete item method will cancel and they can return 
+            if (int.TryParse(input, out int itemNumber))
+            {
+                if (itemNumber == 0)
+                {
+                    Console.WriteLine("Delete operation cancelled.");
+                    return;
+                }
+                else if (itemNumber > 0 && itemNumber <= foodBank.Count)
+                {
+                    string deletedItemName = foodBank[itemNumber - 1].foodName;
+                    foodBank.RemoveAt(itemNumber - 1);
+                    Console.WriteLine($"\n✓ '{deletedItemName}' has been removed from inventory.");
+                }
+                else
+                {
+                    Console.WriteLine($"\nInvalid selection. Please enter a number between 1 and {foodBank.Count}.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid input. Please enter a number.");
+            }
+        }
+
+        // This prints the list of all the items in the inventory
+        static void inventoryList(List<FoodItem> foodBank)
+        {
+            Console.WriteLine("Food Bank Inventory: ");
+
+            // if foodBank is empty display this text
+            if (foodBank.Count == 0)
+            {
+                Console.WriteLine("\nThe inventory is currently empty.");
+                return;
+            }
+
+            for (int i = 0; i < foodBank.Count; i++)
+            {
+                Console.WriteLine($"Item #{i + 1}:");
+                foodBank[i].showItems();
+            }
+        }
+    }
+}
